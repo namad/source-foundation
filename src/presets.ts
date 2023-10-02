13e1b8cfb2989a827c6758a1acd85a30c819a79f
Pavel Kiselev <pk.kiselev@gmail.com>
2023-10-02 16:53:04 +0100

    added few more presets

--- src/presets.ts
+++ src/presets.ts
@@ -1,6 +1,9 @@
 import theme1 from './presets/default.json';
 import theme2 from './presets/royal-blue.json';
 import theme3 from './presets/cyan.json';
+import theme4 from './presets/indigo.json';
+import theme5 from './presets/pink.json';
+
 import { ImportFormData } from './import';
 
 
@@ -8,42 +11,53 @@ export function getPresets(): ImportFormData[] {
     return [
         theme1 as ImportFormData, 
         theme2 as ImportFormData, 
-        theme3 as ImportFormData
+        theme3 as ImportFormData,
+        theme4 as ImportFormData,
+        theme5 as ImportFormData,
     ]
 }
 
 export function getPresetContentTemplate(index) {
     return `
         <input type="radio" name="theme" value="${index}" />
-        <span class="text-base hidden">Theme ${index}</span>
-        <span class="info">
+        <span class="col-info">
             <span class="info-item">
-                <span class="icon icon__spacing" title="Spacing"></span>                
-                <span class="text-text-base-600" data-value="spacing"></span>
+                <span class="icon icon-16 icon-colors" title="Color pattern"></span>                
+                <span>
+                    <span class="text-text-base-600" data-value="distance">2</span><span class="text-text-base-400">%</span>
+                </span>
             </span>
             <span class="info-item">
-                <span class="icon icon__radii" title="Radii"></span>
-                <span class="text-text-base-600" data-value="radii"></span>
+                <span class="icon icon-16 icon-typography" title="Typography"></span>
+                <span>
+                    <span class="text-text-base-600" data-value="typeScale">Minor Third</span> / 
+                    <span class="text-text-base-600" data-value="baseFontSize">Base</span>
+                </span>
             </span>
-            <span class="info-item">
-                <span class="icon icon__typography" title="Typography"></span>
-                <span class="text-text-base-600" data-value="typeScale"></span> / 
-                <span class="text-text-base-600" data-value="baseFontSize"></span>
-            </span>
+            <span class="flex flex-row gap-sx-base">
+                <span class="info-item">
+                    <span class="icon icon-16 icon-spacing" title="Spacing"></span>                
+                    <span class="text-text-base-600" data-value="spacing">Base</span>
+                </span>
+                <span class="info-item">
+                    <span class="icon icon-16 icon-radii" title="Radii"></span>
+                    <span class="text-text-base-600" data-value="radii">Base</span>
+                </span>
+            </span>                
         </span>
-        <span class="swatches neutral-swatches">
-            <span class="swatch swatch-1"></span>
-            <span class="swatch swatch-2"></span>
-            <span class="swatch swatch-3"></span>
-            <span class="swatch swatch-4"></span>
-            <span class="swatch swatch-5"></span>
-        </span>
-        <span class="swatches semantic-swatches">
-            <span class="swatch swatch-1"></span>
-            <span class="swatch swatch-2"></span>
-            <span class="swatch swatch-3"></span>
-            <span class="swatch swatch-4"></span>
-            <span class="swatch swatch-5"></span>
+        <span class="col-swatches">
+            <span class="swatches">
+                <span class="swatch bg-fill-base-200"></span>
+                <span class="swatch bg-fill-base-300"></span>
+                <span class="swatch bg-fill-base-400"></span>
+                <span class="swatch bg-fill-base-500"></span>
+            </span>
+            <span class="swatches">
+                <span class="swatch bg-primary-500"></span>
+                <span class="swatch bg-primary-400"></span>
+                <span class="swatch bg-primary-300"></span>
+                <span class="swatch bg-primary-200"></span>
+            </span>
         </span>
     `;
 }
